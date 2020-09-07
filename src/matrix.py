a66743785163acad98cab89f5b8eba10db134129
JHpusd <justinhongdebate@gmail.com>
2020-09-07 18:25:44 +0000

    put all methods in Matrix class

--- src/matrix.py
+++ src/matrix.py
@@ -3,5 +3,16 @@ class Matrix(input_elements):
         self.elements = input_elements
     
     def copy(self):
-        new_matrix = [[0, 0], [0, 0]]
-        
+        result_matrix = [[0, 0], [0, 0]]
+
+    def add(self, input_matrix):
+        # filler text
+    
+    def subtract(self, input_matrix):
+        # filler text
+    
+    def scalar_multiply(self, input_scalar):
+        # filler text
+    
+    def matrix_multiply(self, input_matrix):
+        # filler text
