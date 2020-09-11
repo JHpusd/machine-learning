884104d7456ed41de2a79631b738e09c98c1565d
JHpusd <justinhongdebate@gmail.com>
2020-09-11 00:13:06 +0000

    finished 7-1

--- src/matrix.py
+++ src/matrix.py
@@ -1,39 +1,68 @@
 class Matrix():
     def __init__(self, input_elements):
         self.elements = input_elements
+        self.num_rows = len(self.elements)
+        self.num_cols = len(self.elements[0])
 
     def copy(self):
-        new_matrix = Matrix(self.elements)
-        return new_matrix
+        return self
 
     def add(self, input_matrix):
-        result = [[0, 0], [0, 0]]
-        for i in range(len(self.elements)):
-            for j in range(len(self.elements[0])):
-                result[i][j] = (
+        result = []
+        for i in range(self.num_rows):
+            result.append([])
+            for j in range(self.num_cols):
+                result[i].append(
                     self.elements[i][j] + input_matrix.elements[i][j])
         return Matrix(result)
 
     def subtract(self, input_matrix):
-        result = [[0, 0], [0, 0]]
-        for i in range(len(self.elements)):
-            for j in range(len(self.elements[0])):
-                result[i][j] = (
+        result = []
+        for i in range(self.num_rows):
+            result.append([])
+            for j in range(self.num_cols):
+                result[i].append(
                     self.elements[i][j] - input_matrix.elements[i][j])
         return Matrix(result)
 
     def scalar_multiply(self, input_scalar):
-        result = [[0, 0], [0, 0]]
-        for i in range(len(self.elements)):
-            for j in range(len(self.elements[0])):
-                result[i][j] = self.elements[i][j] * input_scalar
+        result = []
+        for i in range(self.num_rows):
+            result.append([])
+            for j in range(self.num_cols):
+                result[i].append(
+                    self.elements[i][j] * input_scalar)
         return Matrix(result)
 
     def matrix_multiply(self, input_matrix):
-        result = [[0, 0], [0, 0]]
-        for i in range(len(self.elements)):
-            for j in range(len(input_matrix.elements[0])):
-                for k in range(len(input_matrix.elements)):
+        result = []
+        for i in range(self.num_rows):
+            result.append([])
+            for j in range(input_matrix.num_cols):
+                result[i].append(0)
+        for i in range(self.num_rows):
+            for j in range(input_matrix.num_cols):
+                for k in range(input_matrix.num_rows):
                     result[i][j] += (
                         self.elements[i][k] * input_matrix.elements[k][j])
         return Matrix(result)
+
+    def transpose(self):
+        result = []
+        for i in range(self.num_cols):
+            result.append([])
+            for j in range(self.num_rows):
+                result[i].append(self.elements[j][i])
+        return Matrix(result)
+
+    def is_equal(self, input_matrix):
+        if self.elements == input_matrix.elements:
+            return True
+        else:
+            return False
+    
+    def round(self, decimal_places):
+        for i in self.elements:
+            for j in range(len(i)):
+                i[j] = round(i[j], 5)
+        return Matrix(self.elements)
--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -11,7 +11,7 @@ print("PASSED")
 
 C = Matrix([[1, 0], [2, -1]])
 D = B.add(C)
-print("Test method 'add'...")
+print("Testing method 'add'...")
 assert D.elements == [[2, 3], [4, 3]]
 print("PASSED")
 
@@ -29,3 +29,39 @@ G = B.matrix_multiply(C)
 print("Testing method 'matrix_multiply'...")
 assert G.elements == [[7, -3], [10, -4]]
 print("PASSED")
+
+A = Matrix([[1, 0, 2, 0, 3], [0, 4, 0, 5, 0],
+    [6, 0, 7, 0, 8], [-1, -2, -3, -4, -5]])
+print("Testing attributes 'num_rows' and 'num_cols'...")
+assert (A.num_rows, A.num_cols) == (4, 5)
+print("PASSED")
+
+A_t = A.transpose()
+print("Testing method 'transpose'...")
+assert A_t.elements == [[1, 0, 6, -1], [0, 4, 0, -2],
+    [2, 0, 7, -3], [0, 5, 0, -4], [3, 0, 8, -5]]
+print("PASSED")
+
+print("Testing that methods work on transposed function...")
+B = A_t.matrix_multiply(A)
+assert B.elements == [[38, 2, 47, 4, 56], [2, 20, 6, 28, 10],
+    [47, 6, 62, 12, 77], [4, 28, 12, 41, 20], [56, 10, 77, 20, 98]]
+
+C = B.scalar_multiply(0.1).round(5)
+assert C.elements == [[3.8, .2, 4.7, .4, 5.6], 
+    [.2, 2.0, .6, 2.8, 1.0], [4.7,  .6, 6.2, 1.2, 7.7],
+    [.4, 2.8, 1.2, 4.1, 2.0],[5.6, 1.0, 7.7, 2.0, 9.8]]
+
+D = B.subtract(C)
+assert D.elements == [[34.2, 1.8, 42.3, 3.6, 50.4],
+    [1.8, 18., 5.4, 25.2, 9.], [42.3, 5.4, 55.8, 10.8, 69.3],
+    [3.6, 25.2, 10.8, 36.9, 18.], [50.4, 9., 69.3, 18.,88.2]]
+
+E = D.add(C)
+assert E.elements == [[38, 2, 47, 4, 56], [2, 20, 6, 28, 10],
+    [47, 6, 62, 12, 77], [4, 28, 12, 41, 20],
+    [56, 10, 77, 20, 98]]
+
+assert (E.is_equal(B), E.is_equal(C)) == (True, False)
+
+print("ALL PASSED")
