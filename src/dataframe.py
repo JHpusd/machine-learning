0e40a5ac19fb6b65cbc907355debbbecef03e680
JHpusd <justinhongdebate@gmail.com>
2020-11-01 19:50:21 +0000

    moved dataframe testing

--- src/dataframe.py
+++ src/dataframe.py
@@ -25,24 +25,3 @@ class DataFrame():
                     new_list.append(clone_dict[key][j])
             clone_dict[key] = new_list
         return DataFrame(clone_dict, self.columns)
-
-data_dict = {
-    'Pete': [1, 0, 1, 0],
-    'John': [2, 1, 0, 2],
-    'Sarah': [3, 1, 4, 0]
-}
-
-print("Testing class DataFrame ...")
-df1 = DataFrame(data_dict, ['Pete', 'John', 'Sarah'])
-
-assert df1.data_dict == {'Pete': [1, 0, 1, 0],'John': [2, 1, 0, 2],'Sarah': [3, 1, 4, 0]}
-assert df1.columns == ['Pete', 'John', 'Sarah']
-assert df1.to_array() == [[1, 2, 3], [0, 1, 1], [1, 0, 4], [0, 2, 0]]
-
-df2 = df1.select_columns(['Sarah', 'Pete'])
-assert df2.to_array() == [[3, 1], [1, 0], [4, 1], [0, 0]]
-assert df2.columns == ['Sarah', 'Pete']
-
-df3 = df1.select_rows([1, 3])
-assert df3.to_array() == [[0, 1, 1], [0, 2, 0]]
-print("PASSED")
