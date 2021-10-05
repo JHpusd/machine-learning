b2052a19bac5c13f4b0f2750a1299ae19413ece6
JHpusd <69809321+JHpusd@users.noreply.github.com>
2021-10-05 04:20:03 +0000

    finished cross fold accuracies for decisions tree

--- datasets/random_clusters.py
+++ datasets/random_clusters.py
@@ -7,9 +7,10 @@ o_clusters = [(1,4), (4,1)]
 
 def elem_add(a, b):
     assert len(a) == len(b), "different lengths"
-    return [a[i]+b[i] for i in range(len(a))]
+    elems = [a[i]+b[i] for i in range(len(a))]
+    return tuple(elems)
 
-def generate_clusters(x_centers, o_centers, p, num_points):
+def generate_clusters(x_centers, o_centers, p, max_dist, num_points):
     # num_points is number of extra points for each type
     # total number of added points will be num_points * 2
     if type(x_centers) != list:
@@ -18,20 +19,13 @@ def generate_clusters(x_centers, o_centers, p, num_points):
         o_ceters = [o_centers]
     points = {'x':[], 'o':[]}
 
-    pairs = [[pair[0], pair[1]] for pair in x_centers+o_centers]
-    all_elems = []
-    for pair in pairs:
-        all_elems.append(pair[0])
-        all_elems.append(pair[1])
-    largest_dist = max(all_elems)
-
     for _ in range(num_points):
         x_center = x_centers[r.randint(0,len(x_centers)-1)]
         o_center = o_centers[r.randint(0,len(o_centers)-1)]
 
         for i in range(2):
             dist_scalar = (r.randint(1,100)**p) / 100**p
-            dist = largest_dist * dist_scalar
+            dist = max_dist * dist_scalar
             ang = m.pi*r.randint(0,360) / 180
             translation = (dist*m.cos(ang), dist*m.sin(ang))
             if i == 0:
@@ -41,12 +35,16 @@ def generate_clusters(x_centers, o_centers, p, num_points):
     
     return points
 
-points = generate_clusters([(1,1), (4,4)], [(1,4), (4,1)], 3, 100)
-plt.style.use('bmh')
-#print([p[0] for p in points['x']])
-plt.scatter([p[0] for p in points['x']], [p[1] for p in points['x']], color='red',s=10, marker='x',label='x')
-plt.scatter([p[0] for p in points['o']], [p[1] for p in points['o']], color='#66ff00',s=10,marker='o',label='o')
-plt.xlim(-8, 8)
-plt.ylim(-8, 8)
-plt.legend(loc='lower left')
-plt.savefig('semirandom_clusters.png')
+def show(point_dict, x_lim=None, y_lim=None, filename='semirandom_clusters'):
+    plt.style.use('bmh')
+    plt.clf()
+    for key in point_dict:
+        plt.scatter([p[0] for p in point_dict[key]], [p[1] for p in point_dict[key]],marker=key,s=20)
+    if x_lim != None:
+        plt.xlim(x_lim[0], x_lim[1])
+    if y_lim != None:
+        plt.ylim(y_lim[0], y_lim[1])
+    plt.savefig(filename+'.png')
+
+#points = generate_clusters(x_clusters, o_clusters, 2, 4, 100)
+#show(points, (-8,8), (-8,8))
--- tests/test_decision_tree.py
+++ tests/test_decision_tree.py
@@ -1,6 +1,9 @@
-import sys
+import sys, time
 sys.path.append('src')
 from decision_tree import *
+sys.path.append('datasets')
+from random_clusters import *
+
 '''
 init_points = {'x': [(1,1), (1,2), (2,2), (2,3)], 'o': [(1,3), (1,4), (2,4)]}
 dt = DecisionTree(init_points)
@@ -50,11 +53,10 @@ assert dt.predict((1,2)) == 'x' # random seeded
 assert dt.predict((2,2)) == 'x' # random seeded
 for branch in dt.branches:
     assert len(branch.branches) == 0
-'''
+
 init_points = {'x':[(0,1),(0,1),(0,2),(1,1),(1,2),(1,2)], 'o':[(0,2),(1,1),(1,1),(1,2)]}
 dt= DecisionTree(init_points, 1)
 
-<<<<<<< HEAD
 dt.fit()
 
 assert dt.predict((0,1)) == 'x'
@@ -64,6 +66,87 @@ assert dt.predict((0,2)) == 'x' # random seeded
 for branch in dt.branches:
     for subbranch in branch.branches:
         assert len(subbranch.branches) == 0
-=======
-dt.split(get_best_split())
->>>>>>> origin/master
+'''
+x_clusters = [(1,1), (4,4)]
+o_clusters = [(1,4), (4,1)]
+
+points = generate_clusters(x_clusters, o_clusters, 2, 4, 100)
+points_copy = points.copy()
+
+def dict_remove(input_dict, key, item):
+    copy = input_dict.copy()
+    new_items = copy[key].remove(item)
+    copy[key] = new_items
+    return copy
+
+def get_all_vals(point_dict):
+    result = []
+    for key in point_dict:
+        result += point_dict[key]
+    return result
+
+def random_fold(point_dict, size):
+    keys = list(point_dict.keys())
+    result = {key:[] for key in keys}
+    for _ in range(size):
+        r_key = keys[r.randint(0,len(keys)-1)]
+        if len(point_dict[r_key]) == 0:
+            keys.remove(r_key)
+            r_key = keys[r.randint(0,len(keys)-1)]
+        items = point_dict[r_key]
+        r_item = items[random.randint(0,len(items)-1)]
+        result[r_key].append(r_item)
+        dict_remove(point_dict, r_key, r_item)
+    return result
+
+def mult_folds(num_times, point_dict):
+    fold_dicts = []
+    fold_size = len(get_all_vals(point_dict)) / num_times
+    assert int(fold_size) == fold_size, "num_times is incompatible"
+    for _ in range(num_times):
+        fold_dicts.append(random_fold(point_dict, int(fold_size)))
+    return fold_dicts
+
+def add_point_dicts(point_dict_list):
+    # too lazy to add any more asserts
+    result = {key:[] for key in point_dict_list[0]}
+    for point_dict in point_dict_list:
+        for key in point_dict:
+            result[key] += point_dict[key]
+    return result
+
+def correct_percent(fitted_tree, test_point_dict):
+    total = 0
+    correct = 0
+    for key in test_point_dict:
+        for coord in test_point_dict[key]:
+            if fitted_tree.predict(coord) == key:
+                correct += 1
+            total += 1
+    return correct / total
+
+def get_training(all_folds, excluded_fold):
+    point_dict_list = [fold for fold in all_folds if fold != excluded_fold]
+    return add_point_dicts(point_dict_list)
+
+folds = mult_folds(5, points_copy)
+
+min_sizes = [1,2,5,10,15,20,30,50,100]
+correct_percentages = []
+for min_size in min_sizes:
+    avg_correct = 0
+    for fold in folds:
+        train = get_training(folds, fold)
+        dt = DecisionTree(train, min_size)
+        dt.fit()
+        avg_correct += correct_percent(dt, fold)
+    avg_correct /= len(folds)
+    correct_percentages.append(avg_correct)
+
+plt.style.use('bmh')
+plt.plot(min_sizes, correct_percentages)
+plt.xlim(1,100)
+plt.ylim(0,1)
+plt.xlabel('min_size_to_split')
+plt.ylabel('5-fold accuracy')
+plt.savefig('min_size_vs_accuracy.png')
