1ffd5e7cc8b868ef1a3e71b25d9642062f214cf0
JHpusd <69809321+JHpusd@users.noreply.github.com>
2021-10-05 17:44:14 +0000

    moved helper functions to src file

--- tests/test_decision_tree.py
+++ tests/test_decision_tree.py
@@ -73,62 +73,6 @@ o_clusters = [(1,4), (4,1)]
 points = generate_clusters(x_clusters, o_clusters, 2, 4, 100)
 points_copy = points.copy()
 
-def dict_remove(input_dict, key, item):
-    copy = input_dict.copy()
-    new_items = copy[key].remove(item)
-    copy[key] = new_items
-    return copy
-
-def get_all_vals(point_dict):
-    result = []
-    for key in point_dict:
-        result += point_dict[key]
-    return result
-
-def random_fold(point_dict, size):
-    keys = list(point_dict.keys())
-    result = {key:[] for key in keys}
-    for _ in range(size):
-        r_key = keys[r.randint(0,len(keys)-1)]
-        if len(point_dict[r_key]) == 0:
-            keys.remove(r_key)
-            r_key = keys[r.randint(0,len(keys)-1)]
-        items = point_dict[r_key]
-        r_item = items[random.randint(0,len(items)-1)]
-        result[r_key].append(r_item)
-        dict_remove(point_dict, r_key, r_item)
-    return result
-
-def mult_folds(num_times, point_dict):
-    fold_dicts = []
-    fold_size = len(get_all_vals(point_dict)) / num_times
-    assert int(fold_size) == fold_size, "num_times is incompatible"
-    for _ in range(num_times):
-        fold_dicts.append(random_fold(point_dict, int(fold_size)))
-    return fold_dicts
-
-def add_point_dicts(point_dict_list):
-    # too lazy to add any more asserts
-    result = {key:[] for key in point_dict_list[0]}
-    for point_dict in point_dict_list:
-        for key in point_dict:
-            result[key] += point_dict[key]
-    return result
-
-def correct_percent(fitted_tree, test_point_dict):
-    total = 0
-    correct = 0
-    for key in test_point_dict:
-        for coord in test_point_dict[key]:
-            if fitted_tree.predict(coord) == key:
-                correct += 1
-            total += 1
-    return correct / total
-
-def get_training(all_folds, excluded_fold):
-    point_dict_list = [fold for fold in all_folds if fold != excluded_fold]
-    return add_point_dicts(point_dict_list)
-
 folds = mult_folds(5, points_copy)
 
 min_sizes = [1,2,5,10,15,20,30,50,100]
